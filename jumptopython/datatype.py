4f65b387c60658fc2c84f008923224e9788eb37f
JangWony <jwon2@snu.ac.kr>
2019-08-28 01:10:19 +0900

    Start datatype

---
+++ jumptopython/datatype.py
@@ -0,0 +1,15 @@
+# numeric type
+## Integer
+Int_a = 123
+Int_b = -123
+Int_c = 0
+## float
+float_a = 1.2
+float_b = -1.23
+float_c = 1.23E10
+float_d = 1.23E-10
+## Octal
+Oct_a = 0o123
+## Hex
+Hex_a = 0x1fe
+## Operation
